e3c026cdccd6fdda2bbbce0bd0da0571f56cf942
riiiiistan <sangangbayant@gmail.com>
2022-04-18 08:52:46 +0800

    Clean commit2

--- webpack.mix.js
+++ webpack.mix.js
@@ -23,16 +23,3 @@ mix.js("resources/js/app.js", "public/js")
 if (mix.inProduction()) {
     mix.version();
 }
-
-mix.browserSync({
-    proxy: "http://127.0.0.1:8000",
-});
-
-const { VuetifyLoaderPlugin } = require("vuetify-loader");
-
-module.exports = {
-    plugins: [
-        new VuetifyLoaderPlugin({ autoImport: true }), // Enabled by default
-        new VuetifyLoaderPlugin({ styles: 'expose' }),
-    ],
-};
